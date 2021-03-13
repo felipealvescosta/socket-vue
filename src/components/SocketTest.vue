58bb1eb463f27678e5925706f610df55a23346a3
Felipe Alves <felipealves@MacBook-Pro-de-SAS.local>
2021-03-13 10:34:34 -0300

    first commit

---
+++ src/components/SocketTest.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+    <div id="main-content" class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <form class="form-inline">
+            <div class="form-group">
+              <label for="connect">WebSocket connection:</label>
+              <button id="connect" type="submit" :disabled="connected == true" @click.prevent="connect">Connect</button>
+              <button id="disconnect" type="submit" :disabled="connected == false" @click.prevent="disconnect">Disconnect
+              </button>
+            </div>
+          </form>
+        </div>
+        <div class="col-md-6">
+          <form class="form-inline">
+            <div class="form-group">
+              <label for="name">What is your name?</label>
+              <input type="text" id="name" class="form-control" v-model="send_message" placeholder="Your name here...">
+            </div>
+            <button id="send" type="submit" @click.prevent="send">Send</button>
+          </form>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <table id="conversation" class="table table-striped">
+            <thead>
+              <tr>
+                <th>Greetings</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in received_messages" :key="item">
+                <td>{{ item }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import SockJS from "sockjs-client";
+import Stomp from "webstomp-client";
+
+export default {
+  name: "SocketTest",
+  data() {
+    return {
+      received_messages: [],
+      send_message: null,
+      connected: false
+    };
+  },
+  methods: {
+    send() {
+      console.log("Send message:" + this.send_message);
+      if (this.stompClient && this.stompClient.connected) {
+        const msg = { name: this.send_message };
+        console.log(JSON.stringify(msg));
+        this.stompClient.send("/app/ws/foo/send", JSON.stringify(msg), {});
+      }
+    },
+    connect() {
+      this.socket = new SockJS("http://7d43db2c96eb.ngrok.io/ws");
+      this.stompClient = Stomp.over(this.socket);
+      this.stompClient.connect(
+        {},
+        frame => {
+          this.connected = true;
+          console.log(frame);
+          this.stompClient.subscribe("/topic/messages/foo", tick => {
+            console.log(tick);
+            this.received_messages.push(JSON.parse(tick.body).content);
+            console.log(this.received_messages)
+          });
+        },
+        error => {
+          console.log(error);
+          this.connected = false;
+        }
+      );
+    },
+    disconnect() {
+      if (this.stompClient) {
+        this.stompClient.disconnect();
+      }
+      this.connected = false;
+    },
+    tickleConnection() {
+      this.connected ? this.disconnect() : this.connect();
+    }
+  },
+  mounted() {
+    // this.connect();
+  }
+};
+</script>
+<style scoped="">
+</style>
